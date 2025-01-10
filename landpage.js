2129eaad7e9aa4be4d365530753ba2778e37fb86
Stephie-BTRSD <biloguistephie@gmail.com>
2025-01-10 20:52:10 +0100

    Add files via upload
    i have modified the java code to display some images in the background though i still have some challenges

--- landpage.js
+++ landpage.js
@@ -1,4 +1,22 @@
-document.getElementById('startButton').addEventListener('click', () => {
-    alert('Game Started!');
-});
+async function changeBackgroundImages() {
+    const images = await import('./images-folder/images.js');
+
+    let currentImageIndex = 0;
+    const backgroundElement = document.createElement('div');
+    backgroundElement.classList.add('background');
+    document.body.appendChild(backgroundElement);
+    backgroundElement.style.backgroundImage = `url(${images[currentImageIndex]})`;
+
+    function changeBackgroundImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        backgroundElement.style.backgroundImage = `url(${images[currentImageIndex]})`;
+    }
+
+    setInterval(changeBackgroundImage, 5000); // Change image every 5 seconds
+}
+
+changeBackgroundImages();
+
+
+
 
